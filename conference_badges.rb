bddc7b5e476d68875a6de80f23a412ea8c1b40fa
jessi ortega <ortegabx05@gmail.com>
2019-07-03 18:25:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
+def batch_badge_creator(speakers)
+  badges=[]
+  speakers.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
+  end
+  return badges
